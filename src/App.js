17a9864cd477a019cbbf9abbc21bd8a3706d6076
arabfysal <arabfysal@gmail.com>
2023-02-21 21:34:02 +0100

    create section component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
   Select,
   Grid,
   GridItem,
+  Center,
+  Button,
 } from '@chakra-ui/react';
 import theme from './theme';
 import { THEME } from './utils/constants';
 import { HeaderComponent } from './components/Header';
 // import Pricing from './pages/pricing';
 import { MyCard } from './components/Card';
+import Section from './components/Section';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -70,17 +72,11 @@ function App() {
           mx="auto"
           bg={THEME.PrimaryGray}
         >
-          <Box maxW="50%" mx="auto" display="flex" flexDirection="column">
-            <Box textAlign={'center'}>
-              <Text fontSize={52} align="center" as="b">
-                Pricing for every business{' '}
-              </Text>
-              <Text fontSize={18}>
-                No hidden fees or charges - simple & flexible pricing that fits
-                with your business at any stage.
-              </Text>
-            </Box>
-            <Box maxH="160px">
+         <Section
+         title="Pricing for every business"
+         description="No hidden fees or charges - simple & flexible pricing that fits with your business at any stage."
+         action={
+          <Box maxH="160px">
               <Grid templateColumns="repeat(2, 1fr)" gap={6}>
                 <GridItem>
                   <Select
@@ -124,7 +120,8 @@ function App() {
                 </GridItem>
               </Grid>
             </Box>
-          </Box>
+         }
+         />
 
           {/* <Pricing/> */}
         </Box>
@@ -140,11 +137,65 @@ function App() {
           <Grid templateColumns="repeat(3, 1fr)" gap={6} mt={20}>
             {Object?.keys(price)?.map(item => (
               <GridItem w="100%">
-                <MyCard label={item} data={price} currency={selectedCurrency?.name} />
+                <MyCard
+                  label={item}
+                  data={price}
+                  currency={selectedCurrency?.name}
+                />
               </GridItem>
             ))}
           </Grid>
-          {/* second section */}
+        </Box>
+        <Box
+          display="flex"
+          flexDirection="column"
+          w="100vw"
+          p="100px 0"
+          px={32}
+          mx="auto"
+          bg={THEME.SecondaryGray}
+        >
+          <Section
+            title="Sendchamp for Startups"
+            description="Apply for $1,000 in credits, if you are a startup that is less than 3 years old with less than $500k in total funding."
+            action={<Center>
+              <Button bg={THEME.PrimaryBlue}>Apply Now</Button>
+            </Center>}
+          />
+        </Box>
+        <Box
+          display="flex"
+          flexDirection="column"
+          w="100vw"
+          p="100px 0"
+          px={32}
+          mx="auto"
+          bg="#fff"
+        >
+          <Section
+            title="Start building better communication experience with Sendchamp"
+            description="Access a platform that is modernizing communications and making it possible for your customers to communicate with your business the same way they do with their friends and family."
+            action={
+              <>
+                <Grid templateColumns="repeat(2, 1fr)" gap={2}>
+                  <GridItem>
+                    <Button
+                      bg={THEME.PrimaryBlue}
+                    >
+                      Start for free
+                    </Button>
+                  </GridItem>
+                  <GridItem>
+                    <Button
+                      bg={THEME.PrimaryBlue}
+                    >
+                      Talk To Sales
+                    </Button>
+                  </GridItem>
+                </Grid>
+              </>
+            }
+          />
         </Box>
       </Box>
     </ChakraProvider>
