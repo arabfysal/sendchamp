7f79653c08d4af184c73091fba06964ab89f70b6
arabfysal <arabfysal@gmail.com>
2023-02-21 16:35:36 +0100

    config global style

---
+++ src/theme.jsx
@@ -0,0 +1,22 @@
+import { extendTheme } from '@chakra-ui/react';
+
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        fontFamily: 'body',
+        color: 'gray.800',
+        bg: '#fff',
+        margin: 0,
+        padding: 0,
+      },
+    },
+  },
+  fonts: {
+    body: 'DM Sans, sans-serif',
+    heading: 'DM Sans, sans-serif',
+  },
+});
+
+export default theme;
